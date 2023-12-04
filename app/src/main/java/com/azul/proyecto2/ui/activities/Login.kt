864d61dc5cdd28aa8ec4c86e7f792a59843aa1c0
Azul AF <azulazulada8@gmail.com>
2023-12-03 20:31:26 -0600

    Version corregida de strings y edicion en el splashscreen

--- app/src/main/java/com/azul/proyecto2/ui/activities/Login.kt
+++ app/src/main/java/com/azul/proyecto2/ui/activities/Login.kt
@@ -58,21 +58,21 @@ class Login : AppCompatActivity() {
                 resetEmail.inputType = InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
 
                 val passwordResetDialog = AlertDialog.Builder(it.context)
-                    .setTitle("Restablecer contraseña")
-                    .setMessage("Ingresa el correo para restablecer su contraseña")
+                    .setTitle(getString(R.string.Restablecer))
+                    .setMessage(getString(R.string.correo_instr))
                     .setView(resetEmail)
-                    .setPositiveButton("Enviar"){ _, _ ->
+                    .setPositiveButton(getString(R.string.send)){ _, _ ->
                         val mail = resetEmail.text.toString()
                         if(mail.isNotEmpty()){
                             firebaseAuth.sendPasswordResetEmail(mail).addOnSuccessListener {
-                                Toast.makeText(this@Login, "@strings/corr_contra_send", Toast.LENGTH_SHORT).show()
+                                Toast.makeText(this@Login, getString(R.string.corr_contra_send), Toast.LENGTH_SHORT).show()
                             }.addOnFailureListener {
-                                Toast.makeText(this@Login, "@strings/mail_not_sent", Toast.LENGTH_SHORT).show()
+                                Toast.makeText(this@Login, getString(R.string.mail_not_sent), Toast.LENGTH_SHORT).show()
                             }
                         }else{
-                            Toast.makeText(this@Login, "@strings/enter_mail", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this@Login, getString(R.string.enter_mail), Toast.LENGTH_SHORT).show()
                         }
-                    }.setNegativeButton("Cancelar"){ dialog, _ ->
+                    }.setNegativeButton(getString(R.string.cancel)){ dialog, _ ->
                         dialog.dismiss()
                     }
                     .create()
@@ -86,13 +86,13 @@ class Login : AppCompatActivity() {
         contrasenia = binding.tietContrasenia.text.toString().trim()
 
         if(email.isEmpty()){
-            binding.tietEmail.error = "Se requiere el correo"
+            binding.tietEmail.error = getString(R.string.requiere_correo)
             binding.tietEmail.requestFocus()
             return false
         }
 
         if(contrasenia.isEmpty() || contrasenia.length < 6){
-            binding.tietContrasenia.error = "Se requiere una contraseña o la contraseña no tiene por lo menos 6 caracteres"
+            binding.tietContrasenia.error = getString(R.string.contra_instr)
             binding.tietContrasenia.requestFocus()
             return false
         }
@@ -113,42 +113,42 @@ class Login : AppCompatActivity() {
         //No funciona el programa
         when(errorCode){
             "ERROR_INVALID_EMAIL" -> {
-                Toast.makeText(this, "@strings/mail_error_format", Toast.LENGTH_SHORT).show()
-                binding.tietEmail.error = "Error: El correo electrónico no tiene un formato correcto"
+                Toast.makeText(this, getString(R.string.mail_error_format), Toast.LENGTH_SHORT).show()
+                binding.tietEmail.error = getString(R.string.mail_error_format)
                 binding.tietEmail.requestFocus()
             }
             "ERROR_WRONG_PASSWORD" -> {
-                Toast.makeText(this, "@strings/invalid_password", Toast.LENGTH_SHORT).show()
-                binding.tietContrasenia.error = "Error: La contraseña no es válida"
+                Toast.makeText(this, getString(R.string.invalid_password), Toast.LENGTH_SHORT).show()
+                binding.tietContrasenia.error = getString(R.string.invalid_password)
                 binding.tietContrasenia.requestFocus()
                 binding.tietContrasenia.setText("")
 
             }
             "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL" -> {
                 //An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address.
-                Toast.makeText(this, "@strings/dif_account", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.dif_account), Toast.LENGTH_SHORT).show()
             }
             "ERROR_EMAIL_ALREADY_IN_USE" -> {
-                Toast.makeText(this, "@strings/another_account", Toast.LENGTH_LONG).show()
-                binding.tietEmail.error = ("Error: el correo electrónico ya está en uso con otra cuenta")
+                Toast.makeText(this, getString(R.string.another_account), Toast.LENGTH_LONG).show()
+                binding.tietEmail.error = getString(R.string.another_account)
                 binding.tietEmail.requestFocus()
             }
             "ERROR_USER_TOKEN_EXPIRED" -> {
-                Toast.makeText(this, "@strings/session_exp", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, getString(R.string.session_exp), Toast.LENGTH_LONG).show()
             }
             "ERROR_USER_NOT_FOUND" -> {
-                Toast.makeText(this, "@strings/no_user", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, getString(R.string.no_user), Toast.LENGTH_LONG).show()
             }
             "ERROR_WEAK_PASSWORD" -> {
-                Toast.makeText(this, "@strings/invalid_pass", Toast.LENGTH_LONG).show()
-                binding.tietContrasenia.error = "La contraseña porporcionada es inválida"
+                Toast.makeText(this, getString(R.string.invalid_pass), Toast.LENGTH_LONG).show()
+                binding.tietContrasenia.error = getString(R.string.invalid_pass)
                 binding.tietContrasenia.requestFocus()
             }
             "NO_NETWORK" -> {
-                Toast.makeText(this, "@strings/no_network_av", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, getString(R.string.no_network_av), Toast.LENGTH_LONG).show()
             }
             else -> {
-                Toast.makeText(this, "Error. No se pudo autenticar exitosamente", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.failed_auth), Toast.LENGTH_SHORT).show()
             }
         }
 
@@ -161,7 +161,7 @@ class Login : AppCompatActivity() {
             binding.progressBar.visibility = View.GONE
 
             if(authResult.isSuccessful){
-                Toast.makeText(this, "@strings/correct_auth", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.correct_auth), Toast.LENGTH_SHORT).show()
                 startActivity(Intent(this, MainActivity::class.java))
                 finish()
             }else{
@@ -180,12 +180,12 @@ class Login : AppCompatActivity() {
                 val user_fb = firebaseAuth.currentUser
 
                 user_fb?.sendEmailVerification()?.addOnSuccessListener {
-                    Toast.makeText(this, "@strings/ver_email_sent", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, getString(R.string.ver_email_sent), Toast.LENGTH_SHORT).show()
                 }?.addOnFailureListener {
-                    Toast.makeText(this, "@strings/mail_error", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, getString(R.string.mail_error), Toast.LENGTH_SHORT).show()
                 }
 
-                Toast.makeText(this, "@strings/user_create", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.user_create), Toast.LENGTH_SHORT).show()
                 startActivity(Intent(this, MainActivity::class.java))
                 finish()
             }else{
